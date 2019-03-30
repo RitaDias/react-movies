42b58ea332f57b7da51a858c4016abf9e144f750
Rita <anarita151@gmail.com>
2019-03-30 14:54:48 +0000

    Add flip on posters

--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -1,17 +1,46 @@
 import React, { Component } from 'react';
+import { Flipper, Flipped } from 'react-flip-toolkit';
 import '../Movies.css';
 
 class Movies extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            currentMovie: null,
+            fullScreen: false
+        }
+
+        this.toggleFullScreen = this.toggleFullScreen.bind(this);
+    }
+
+    toggleFullScreen(movieID) {
+        this.setState(prevState => ({
+            currentMovie: movieID,
+            fullScreen: !prevState.fullScreen
+        }));
+      };
 
     render() {
         return (
-            <div className="gallery">
+            <div className={"gallery " + (this.state.fullScreen ? "prevent-scroll" : "")}>
                 {
                     this.props.movies.map((movie => 
-                    <img 
-                        key={movie.id}
-                        src={"https://image.tmdb.org/t/p/w500/" + movie.poster_path} 
-                        className="movie-poster"></img> 
+                        
+                        <Flipper key={movie.id} flipKey={this.state.fullScreen}>
+                            <Flipped flipId="square">
+                            <div className={((this.state.fullScreen && this.state.currentMovie === movie.id) ? "full-screen-square" : "")}>
+                            
+                                <img 
+                                    key={movie.id}
+                                    src={"https://image.tmdb.org/t/p/w500/" + movie.poster_path} 
+                                    className="movie-poster"
+                                    onClick={() => this.toggleFullScreen(movie.id)}
+                                    ></img> 
+                            </div>
+                            </Flipped>
+                        </Flipper>
+                    
                     ))
                 }
             </div>
